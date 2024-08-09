bc8e3ffa61a20b41dec05a64a3ab02390c62dde0
Dami <damilola.aina18@gmail.com>
2024-08-08 23:46:34 -0400

    .

--- website/src/main_modules/about.jsx
+++ website/src/main_modules/about.jsx
@@ -17,7 +17,10 @@ export function About(){
                     noun &#xB7; software engineer
                 </h3>
                 <p>
-                    <span>[1]</span> My name is Damilola Aina and I am a third-year undergraduate student at the University of Toronto, majoring in Computer Engineering with a minor in Artificial Intelligence. <span>[2]</span> Passionate about leveraging technology to solve complex problems and create innovative solutions. Interest are mainly in <span>software & web development</span> and <span>machine learning</span>. <span>[3]</span>A member of UTSM (University of Toronto Syper-Mileage) where I contribute towards the development of energy-efficient vehicle prototypes
+                    <span>[1]</span> My name is Damilola Aina and I am a third-year undergraduate student at the University of Toronto, majoring in Computer Engineering with a minor in Artificial Intelligence. 
+                    <span>[2]</span> Passionate about leveraging technology to solve complex problems and create innovative solutions. Interest are mainly in <span>software & web development</span> and <span>machine learning</span>. 
+                    <span>[3]</span>A member of UTSM (University of Toronto Syper-Mileage) where I contribute towards the development of energy-efficient vehicle prototypes
+                    <span>[4]</span> I am actively involved in various projects and in my free time, I enjoy coding, exploring new technologies, and participating in hackathons
                 </p>
             </div>
         </div>
